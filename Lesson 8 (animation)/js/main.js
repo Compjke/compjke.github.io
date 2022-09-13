9763e1987f046f05cbbf0cbf900ef99f0a8ffaaa
Alexsander <108433956+Compjke@users.noreply.github.com>
2022-09-13 15:37:35 +0300

    Add files via upload

---
+++ Lesson 8 (animation)/js/main.js
@@ -0,0 +1,10 @@
+// function changeItem() {
+//    document.getElementsByClassNam['.team-images'].style.filter = 'brightness(.5)';
+//  }// функция, которая сработает при наведении.
+//  //она означает - выбрать элемент к Id у которого надо что-то изменить.
+//  // когда в скобки где написано one-two добавите Id своего элемента
+//  function rechangeItem() {
+//    document.getElementsByClassName['.team-images'].style.filter = 'brightness(1)';
+//  }// тут всё также. но наобарот. протсес происходящий про отводе курсора.
+
+
